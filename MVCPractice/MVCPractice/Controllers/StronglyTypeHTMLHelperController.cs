7422e3772e1d4aa1d22b1b767350f8a365b750db
Shekhar <shekhar@gmail.com>
2018-10-03 17:25:08 +0530

    inbuilt html helpers

---
+++ MVCPractice/MVCPractice/Controllers/StronglyTypeHTMLHelperController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractice.Controllers
+{
+    public class StronglyTypeHTMLHelperController : Controller
+    {
+        // GET: StronglyTypeHTMLHelper
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
