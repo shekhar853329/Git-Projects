5ed8c6e5608edb5058b2ff18d1baa1e25d17606e
Shekhar <shekhar@gmail.com>
2018-10-03 14:04:22 +0530

    file added

---
+++ MVCPractice/MVCPractice/Controllers/BuiltInHTMLHelperController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractice.Controllers
+{
+    public class BuiltInHTMLHelperController : Controller
+    {
+        // GET: BuiltInHTMLHelper
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AboutUs(string name=null)
+        {
+            return View();
+        }
+    }
+}
